9e032e2c5e74b590a8885441420f1e40fa1dc386
Daniel John Causer <DanielCauser@users.noreply.github.com>
2019-02-26 17:48:01 -0500

    [DC] API controller folder

---
+++ DataBaseSeederAPI/Controllers/ContractsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DataBaseSeeder.Models;
+using DataBaseSeederAPI;
+
+namespace DataBaseSeederAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractsController : ControllerBase
+    {
+        private readonly ClientContext _context;
+
+        public ContractsController(ClientContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contracts
+        [HttpGet]
+        public IEnumerable<Contract> GetContracts()
+        {
+            return _context.Contracts;
+        }
+
+        // GET: api/Contracts/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetContract([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var contract = await _context.Contracts.FindAsync(id);
+
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contract);
+        }
+
+        // PUT: api/Contracts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContract([FromRoute] Guid id, [FromBody] Contract contract)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != contract.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contract).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContractExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contracts
+        [HttpPost]
+        public async Task<IActionResult> PostContract([FromBody] Contract contract)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Contracts.Add(contract);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContract", new { id = contract.Id }, contract);
+        }
+
+        // DELETE: api/Contracts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContract([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var contract = await _context.Contracts.FindAsync(id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contracts.Remove(contract);
+            await _context.SaveChangesAsync();
+
+            return Ok(contract);
+        }
+
+        private bool ContractExists(Guid id)
+        {
+            return _context.Contracts.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DataBaseSeederAPI/Controllers/ReceiptsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DataBaseSeeder.Models;
+using DataBaseSeederAPI;
+
+namespace DataBaseSeederAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiptsController : ControllerBase
+    {
+        private readonly ClientContext _context;
+
+        public ReceiptsController(ClientContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Receipts
+        [HttpGet]
+        public IEnumerable<Receipt> GetReceipts()
+        {
+            return _context.Receipts;
+        }
+
+        // GET: api/Receipts/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetReceipt([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var receipt = await _context.Receipts.FindAsync(id);
+
+            if (receipt == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(receipt);
+        }
+
+        // PUT: api/Receipts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReceipt([FromRoute] Guid id, [FromBody] Receipt receipt)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != receipt.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(receipt).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReceiptExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Receipts
+        [HttpPost]
+        public async Task<IActionResult> PostReceipt([FromBody] Receipt receipt)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Receipts.Add(receipt);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReceipt", new { id = receipt.Id }, receipt);
+        }
+
+        // DELETE: api/Receipts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReceipt([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var receipt = await _context.Receipts.FindAsync(id);
+            if (receipt == null)
+            {
+                return NotFound();
+            }
+
+            _context.Receipts.Remove(receipt);
+            await _context.SaveChangesAsync();
+
+            return Ok(receipt);
+        }
+
+        private bool ReceiptExists(Guid id)
+        {
+            return _context.Receipts.Any(e => e.Id == id);
+        }
+    }
+}
