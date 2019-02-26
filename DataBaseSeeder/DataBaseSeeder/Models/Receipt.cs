be112d8fd1eb1218647b48ab96a0846aaace3247
Daniel Causer <danielcauser@gmail.com>
2019-02-26 11:47:37 -0500

    [DC] Project changes

--- DataBaseSeeder/DataBaseSeeder/Models/Receipt.cs
+++ DataBaseSeeder/DataBaseSeeder/Models/Receipt.cs
@@ -3,8 +3,10 @@ namespace DataBaseSeeder.Models
 {
     public class Receipt
     {
-        public Receipt()
-        {
-        }
+        public Guid Id { get; set; }
+        public string Detail { get; set; }
+        public Guid ClientId { get; set; }
+        public ReceiptContractStatus ContractStatus { get; set; }
+        public string Balance { get; set; }
     }
 }
