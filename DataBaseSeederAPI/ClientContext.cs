090c177875d092c3c8b17e14dbfc7109fec176cd
Daniel Causer <danielcauser@gmail.com>
2019-02-26 12:17:40 -0500

    [DC] Adding swagger

---
+++ DataBaseSeederAPI/ClientContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DataBaseSeeder.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseSeederAPI
+{
+    public class ClientContext : DbContext
+    {
+        public ClientContext(DbContextOptions<ClientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<ContractNote> ContractNotes { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Seed
+            //var list = new List<TodoItem>();
+
+            //using (var reader = new StreamReader(Assembly.GetAssembly(typeof(Startup))
+            //                                         .GetManifestResourceStream(Assembly.GetAssembly(typeof(Startup)).GetManifestResourceNames().First()) ?? throw new InvalidOperationException()))
+            //{
+            //    list = JsonConvert.DeserializeObject<List<TodoItem>>(reader.ReadToEnd());
+            //}
+
+            //modelBuilder.Entity<TodoItem>().HasData(list.ToArray());
+        }
+    }
+}
--- DataBaseSeederAPI/Startup.cs
+++ DataBaseSeederAPI/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DataBaseSeederAPI
 {
@@ -26,21 +28,27 @@ namespace DataBaseSeederAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ClientContext>(opt =>
+                opt.UseInMemoryDatabase("ClientDb"));
+
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" }); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
-            app.UseHttpsRedirection();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseMvc();
         }
     }
