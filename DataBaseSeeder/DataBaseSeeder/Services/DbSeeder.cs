d13bde5861991d64db12108895b6f9cecb4ffba4
Daniel Causer <danielcauser@gmail.com>
2019-04-06 14:18:56 -0400

    [DC] Getting all classes for a given namespace

--- DataBaseSeeder/DataBaseSeeder/Services/DbSeeder.cs
+++ DataBaseSeeder/DataBaseSeeder/Services/DbSeeder.cs
@@ -1,5 +1,11 @@
 using System;
 using System.Threading.Tasks;
+using SQLitePCL;
+using SQLite;
+using Xamarin.Essentials;
+using DataBaseSeeder.Models;
+using System.Reflection;
+using System.Linq;
 
 namespace DataBaseSeeder
 {
@@ -11,12 +17,38 @@ namespace DataBaseSeeder
         {
             _jsonreaderService = jsonreaderService;
         }
+
+        public SQLiteConnection SQLiteAsyncConnection { get; private set; }
+
         public Task Seed()
         {
             var clients = _jsonreaderService.ReadClientsFromJsonFile();
 
+            //SQLiteAsyncConnection = new SQLiteConnection(FileSystem.AppDataDirectory, "DataBase.db");
+
+            string nspace = "DataBaseSeeder.Models";
+
+            var q = from t in Assembly.GetExecutingAssembly().GetTypes()
+                    where t.IsClass && t.Namespace == nspace
+                    select t;
+            q.ToList().ForEach(t => Console.WriteLine(t.Name));
+            // Reflection + recursion.
+            // for a given type find out all the.
+            // adjacent and get the type from the given list.
+
+            //SQLiteAsyncConnection.CreateTable<Client>();
+            //SQLiteAsyncConnection.CreateTable<Contract>();
+            //SQLiteAsyncConnection.CreateTable<ContractNote>();
+            //SQLiteAsyncConnection.CreateTable<Receipt>();
 
             return Task.CompletedTask;
         }
+
+        // create tables
+
+        // Loop through all entities in payload
+
+        // Save entities to each of the entities 
+        // to the database
     }
 }
