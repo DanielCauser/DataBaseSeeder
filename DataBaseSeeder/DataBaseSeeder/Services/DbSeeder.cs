15bafd0d362cc1b6032be9d28a71e62ea7371420
Daniel Causer <danielcauser@gmail.com>
2019-04-06 15:03:32 -0400

    [DC] solution sketch

--- DataBaseSeeder/DataBaseSeeder/Services/DbSeeder.cs
+++ DataBaseSeeder/DataBaseSeeder/Services/DbSeeder.cs
@@ -6,40 +6,51 @@ using Xamarin.Essentials;
 using DataBaseSeeder.Models;
 using System.Reflection;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace DataBaseSeeder
 {
     public class DbSeeder : IDbSeeder
     {
         private readonly IReadJsonFromFile _jsonreaderService;
+        private List<Type> AlreadyAdded { get; set; }
+        private List<Object> List { get; set; }
 
         public DbSeeder(IReadJsonFromFile jsonreaderService)
         {
             _jsonreaderService = jsonreaderService;
+            AlreadyAdded = new List<Type>(); 
         }
 
-        public SQLiteConnection SQLiteAsyncConnection { get; private set; }
+        public SQLiteConnection sqliteAsyncConnection { get; private set; }
 
         public Task Seed()
         {
-            var clients = _jsonreaderService.ReadClientsFromJsonFile();
-
-            //SQLiteAsyncConnection = new SQLiteConnection(FileSystem.AppDataDirectory, "DataBase.db");
-
+            var list = _jsonreaderService.ReadClientsFromJsonFile();
+            sqliteAsyncConnection = new SQLiteConnection(FileSystem.AppDataDirectory, "DataBase.db");
             string nspace = "DataBaseSeeder.Models";
 
             var q = from t in Assembly.GetExecutingAssembly().GetTypes()
                     where t.IsClass && t.Namespace == nspace
                     select t;
-            q.ToList().ForEach(t => Console.WriteLine(t.Name));
-            // Reflection + recursion.
-            // for a given type find out all the.
-            // adjacent and get the type from the given list.
 
-            //SQLiteAsyncConnection.CreateTable<Client>();
-            //SQLiteAsyncConnection.CreateTable<Contract>();
-            //SQLiteAsyncConnection.CreateTable<ContractNote>();
-            //SQLiteAsyncConnection.CreateTable<Receipt>();
+
+            // for each of the items is the object graph
+
+            // grab all the Nx1 and NxN of tyeps that are in the 
+            // in the assembly type list.
+
+            // create instances for the types
+            // loop thought the lists repeating the steps above
+
+            // exit condition is all the types in the assembly have been added.
+
+
+            q.ToList().ForEach(t => 
+            {
+
+            });
+
 
             return Task.CompletedTask;
         }
